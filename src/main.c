574eec8cfbcfe39692042b240ecdc2480bf3202b
Kamil Zavala Sherby <kamilzavalasherby@gmail.com>
2026-05-24 00:53:10 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -19,7 +19,11 @@ int main(int argc, char *argv[]) {
   if (strcmp(command, "exit") == 0){
     break;
   }
-  
+  if (strncmp(command, "echo ", 5) == 0){
+    printf("%s\n", command + 5);
+    continue;
+  }
+
   printf("%s: command not found\n", command);
 
 
