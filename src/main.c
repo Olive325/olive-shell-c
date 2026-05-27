ca24036bc8e6d31e2fcf38211f98b06549c832a7
Kamil Zavala Sherby <kamilzavalasherby@gmail.com>
2026-05-26 23:01:21 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef void (*CommandFunc)(char *);
+
 typedef struct{
   char *command_name;
-  void (*cmdPtr)();
+  CommandFunc cmdPtr;
 }BuiltinCommands;
 
-void command_exit();
-void command_echo(void *);
-void command_type(void *);
+void command_exit(char *args);
+void command_echo(char *args);
+void command_type(char *args);
 
 const BuiltinCommands builtins[] = {
   {"exit", &command_exit},
-  {"echo ", &command_echo},
-  {"type ", &command_type}
+  {"echo", &command_echo},
+  {"type", &command_type}
 };
 
 #define BUILTINS_LEN 3
@@ -31,74 +33,53 @@ int main(int argc, char *argv[]) {
     char input[1024];
 
   	fgets(input, sizeof(input), stdin);
-
   	input[strcspn(input, "\n")] = '\0';
+  	
+  	char *command = strtok(input, " ");
+  	char *args = strtok(NULL, "");
+  	
+  	if (args == NULL){
+      args = "";
+	}
+	
+	int found = 0;
 
-    for (int i = 0; i <= BUILTINS_LEN; i++){
+    for (int i = 0; i < BUILTINS_LEN; i++){
   	
-  	  int cmd_name_len = sizeof(builtins[i].command_name) - 3;
-  	  
-  	  if (i == BUILTINS_LEN){
-		
-        printf("%s: command not found\n", input);
-	    break;
-	  }
-  	
-  	  if (strncmp(builtins[i].command_name, input, cmd_name_len) == 0){
+  	  if (strcmp(builtins[i].command_name, command) == 0){
   		  
-  		  builtins[i].cmdPtr(&input[cmd_name_len]);
-  		
+  		  builtins[i].cmdPtr(args);
+  		  found = 1;
   		  break;
 	    }
     }
-
+    if (!found){
+      printf("%s:command not found\n", command);
+	}
   };
   return 0; 
 }
 
-void command_exit(){
+void command_exit(char *args){
   exit(0);
 }
 
-void command_echo(void *input_address_){
-  int input_address = *(int*)input_address_;
-  printf("%s\n", input_address_);
+void command_echo(char *args){
+  printf("%s\n", args);
 }
 
-void command_type(void *input_address_){
-  
-  char *input_address = malloc(sizeof((char*)input_address_));
-  
-  if (input_address == NULL){
-  	printf("memory allocation failed");
-  	exit(1);
-  }
-  
-  strcpy(input_address,(char*)input_address_);
-  int input_len = sizeof(input_address);
-  
-  for (int i = 0; i <= BUILTINS_LEN; i++){
-  	
-  	char *current_command = malloc(sizeof(builtins[i].command_name));
-  	
-  	if (current_command == NULL){
-  		printf("memory allocation failed");
-  		exit(1);
-	  }
-	  
-	  strcpy(current_command, builtins[i].command_name);
-	  current_command[strcspn(current_command, " ")] = '\0';
-  	
-  	if (i == BUILTINS_LEN){
-  		printf("%s: not found\n", input_address);
-  		free(input_address);
-  		break;
-	  }
+void command_type(char *args){
 	
-	  if (strncmp(current_command, input_address, input_len) == 0){
-		  printf("%s is a shell builtin\n", input_address);
-		  free(input_address);
-		  break;
-	  }
+  int found = 0;
+  
+  for (int i = 0; i < BUILTINS_LEN; i++){
+	if (strcmp(builtins[i].command_name, args) == 0){
+		printf("%s is a shell builtin\n", args);
+		found = 1;
+		break;
+	}
+  }
+  if (!found){
+	printf("%s:not found\n", args);
   }
 }
